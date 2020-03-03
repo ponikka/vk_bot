723a86c744e20d3e12227505a68c448427190783
Dante <andreev_9000@mail.ru>
2020-03-04 00:00:55 +0700

    day_#2

--- venv/main.py
+++ venv/main.py
@@ -3,16 +3,15 @@ import requests
 from vk_api.bot_longpoll import VkBotEventType, VkBotLongPoll
 from vk_api.utils import get_random_id
 
-vk_session = vk_api.VkApi(token='d7842cff18e3952270798bd8b35995b66ab08ed0b164c5ba0bbfb2e5f4845f87bdeec39d5519ac3815e22')
+vk_session = vk_api.VkApi(token='token')
 vk = vk_session.get_api()
-longpoll = VkBotLongPoll(vk_session, '192612501')
+longpoll = VkBotLongPoll(vk_session, group_id='id')
 
 for event in longpoll.listen():
     if event.type == VkBotEventType.MESSAGE_NEW:
-        if event.obj.text == 'Начать':
+        if event.obj.text != '':
             if event.from_user:
                 vk.messages.send(
-                    user_id = event.user_id,
-                    random_id = get_random_id(),
-                    message = 'Salam'
-                )
+                    user_id=event.obj.from_id,
+                    random_id=get_random_id(),
+                    message=event.obj.text)
