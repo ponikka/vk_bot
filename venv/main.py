ed2fdd1821c6b1f64657d0bd76cf1014274bb4f7
Dante <33492950+ponikka@users.noreply.github.com>
2020-03-04 10:29:35 +0700

    Update main.py

--- venv/main.py
+++ venv/main.py
@@ -3,8 +3,8 @@ import requests
 from vk_api.bot_longpoll import VkBotEventType, VkBotLongPoll
 from vk_api.utils import get_random_id
 
-token = 'd7842cff18e3952270798bd8b35995b66ab08ed0b164c5ba0bbfb2e5f4845f87bdeec39d5519ac3815e22'
-group_id = '192612501'
+token = 'd7842cff18e3952270798bd8b35995b66ab08ed0b164c5ba0bbfb2e5f4845f87bdeec39d5519a2'
+group_id = '1926121'
 vk_session = vk_api.VkApi(token=token)
 vk = vk_session.get_api()
 longpoll = VkBotLongPoll(vk_session, group_id=group_id)
