c52e28344e75c9123a1aee8c1c5d514527cffae9
Dante <andreev_9000@mail.ru>
2020-03-05 23:36:05 +0700

    day_#3

--- main.py
+++ main.py
@@ -2,18 +2,29 @@ import vk_api
 import requests
 from vk_api.bot_longpoll import VkBotEventType, VkBotLongPoll
 from vk_api.utils import get_random_id
+from parcer import weather
 
-token = 'd7842cff18e3952270798bd8b35995b66ab08ed0b164c5ba0bbfb2e5f4845f87bdeec39d5519a2'
-group_id = '1926121'
+
+token = 'd7842cff18e3952270798bd8b35995b66ab08ed0b164c5ba0bbfb2e5f4845f87bdeec39d5519ac3815e22'
+group_id = '192612501'
 vk_session = vk_api.VkApi(token=token)
 vk = vk_session.get_api()
 longpoll = VkBotLongPoll(vk_session, group_id=group_id)
 
+
 for event in longpoll.listen():
     if event.type == VkBotEventType.MESSAGE_NEW:
-        if event.obj.text != '':
+        if event.obj.text == 'Начать':
             if event.from_user:
                 vk.messages.send(
                     user_id=event.obj.from_id,
                     random_id=get_random_id(),
-                    message='kek')
+                    message='Салам')
+
+        if event.obj.text == 'Погода':
+            if event.from_user:
+                vk.messages.send(
+                    user_id=event.obj.from_id,
+                    random_id=get_random_id(),
+                    message=weather())
+
---
+++ parcer.py
@@ -0,0 +1,18 @@
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
+
+
+def weather():
+    html = urlopen('https://yandex.ru/pogoda/krasnoyarsk').read().decode('utf8')
+    soup = BeautifulSoup(html, 'html.parser')
+
+    wthr = soup.find_all('div', attrs={'class': 'temp fact__temp fact__temp_size_s'})
+    wthr = wthr[0]
+
+    weather = wthr.find(class_='temp__value').get_text()
+
+    text = f'In Krasnoyarsk now: {weather}°С'
+
+    return text
+
+print(weather())
